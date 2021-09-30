023e978adde9ebc4c64444122f5883abfb382fb7
Naushad Mahmood <naushad@psybegate.co.za>
2021-09-30 12:33:01 +0200

    Added Repositories

---
+++ src/main/java/com/naushad/bookstore/repository/BookRepository.java
@@ -0,0 +1,9 @@
+package com.naushad.bookstore.repository;
+
+import com.naushad.bookstore.entity.BookEntity;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends CrudRepository<BookEntity, Integer> {
+}
